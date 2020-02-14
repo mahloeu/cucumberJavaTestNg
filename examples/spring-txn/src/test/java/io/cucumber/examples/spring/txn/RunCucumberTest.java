0d1b056ff48473a9d14a10edd014fe5576711695
M.P. Korstanje <rien.korstanje@gmail.com>
2020-02-14 13:48:58 +0100

    [Examples] Use Cucumber JUnit Platform Engine in spring-txn

--- examples/spring-txn/src/test/java/io/cucumber/examples/spring/txn/RunCucumberTest.java
+++ examples/spring-txn/src/test/java/io/cucumber/examples/spring/txn/RunCucumberTest.java
@@ -1,10 +1,7 @@
 package io.cucumber.examples.spring.txn;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions
+@Cucumber
 public class RunCucumberTest {
 }
